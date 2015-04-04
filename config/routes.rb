cd42f1f7ee22387a55020d1f254ea98fbba5403f
shibabu <shibabu.alfa@gmail.com>
2015-04-04 16:17:24 +0900

    added resourceful route for recipes, after trying manual routing (commented out)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
+  root  'pages#home'
 
-  get '/home', to: 'pages#home'
+  get     '/home',                to: 'pages#home'
+
+=begin
+  get     '/recipes',             to: 'recipes#index'
+
+  get     '/recipes/new',         to: 'recipes#new',      as: 'new_recipe'
+
+  post    '/recipes/',            to: 'recipes#create'
+
+  get     '/recipes/:id/edit',    to: 'recipes#edit',     as: 'edit_recipe'
+
+  patch   '/recipes/:id',         to: 'recipes#update'
+
+  get     '/recipes/:id',         to: 'recipes#show',     as: 'recipe'
+
+  delete  '/recipes/:id',         to: 'recipes#destroy'
+=end
+
+  resources :recipes
 
 end
