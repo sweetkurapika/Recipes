d8e80baa0b419f9f8b22a8950e20522b2e2a8d93
shibabu <shibabu.alfa@gmail.com>
2015-04-04 13:49:43 +0900

    add tests and validations for Chef and Recipe Models

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,9 @@
 class Chef < ActiveRecord::Base
-  
+  validates :chefname, presence: true, length: {minimum:3, maximum:40}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+                                    length: {maximum:100},
+                                    format: {with: VALID_EMAIL_REGEX}
+
+  before_save {self.email.downcase!}
 end
