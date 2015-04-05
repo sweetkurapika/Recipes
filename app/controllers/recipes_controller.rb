f9432ffd7e3e96b63a68d842da3072ed2b42500d
shibabu <shibabu.alfa@gmail.com>
2015-04-05 21:55:02 +0900

    add edit and update actions and view with style, plus update the nav helper

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,6 +29,22 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe=Recipe.find params[:id]
+  end
+
+  def update
+    @recipe=Recipe.find params[:id]
+
+    if @recipe.update recipe_params
+      flash[:success]='Recipe successfully updated..'
+
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
 
 
   private
