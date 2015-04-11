b6666386e9bda702b5fe0ecc3fc8db68777ed30b
shibabu <shibabu.alfa@gmail.com>
2015-04-11 22:20:38 +0900

    correct error in picture_uploader helpers ternary operators and setup IDE-environment config for RubyMine

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [600,600]
 
   # Choose what kind of storage to use for this uploader:
-  Rails.env.production? ? storage :fog : storage :file
+  Rails.env.development? ? (storage :file) : (storage :fog)
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
